5ac4293922059b73827c9be5a8d5cda179ceb7a3
Phoebe Lau <phoebelau7@gmail.com>
2018-02-09 23:22:19 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
-  validates :password, length: {minimum: 6}, message: "Password is too short"
+  validates :password, length: {minimum: 6}, :message => "Password is too short"
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
